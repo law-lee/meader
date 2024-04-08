a47144280da1c8cbe8f6047bda713a2e7392bf3d
liliangwei.0401 <liliangwei.0401@bytedance.com>
2024-04-08 19:27:11 +0800

    init

---
+++ cost_level_test.go
@@ -0,0 +1,58 @@
+package meander_test
+
+import (
+	"testing"
+
+	"github.com/cheekybits/is"
+
+	meander "github.com/law-lee/meader"
+)
+
+func TestCostValues(t *testing.T) {
+	isa := is.New(t)
+	isa.Equal(int(meander.Cost1), 1)
+	isa.Equal(int(meander.Cost2), 2)
+	isa.Equal(int(meander.Cost3), 3)
+	isa.Equal(int(meander.Cost5), 5)
+	isa.Equal(int(meander.Cost4), 4)
+}
+
+func TestCostString(t *testing.T) {
+	is := is.New(t)
+	is.Equal(meander.Cost1.String(), "$")
+	is.Equal(meander.Cost2.String(), "$$")
+	is.Equal(meander.Cost3.String(), "$$$")
+	is.Equal(meander.Cost4.String(), "$$$$")
+	is.Equal(meander.Cost5.String(), "$$$$$")
+}
+
+func TestParseCost(t *testing.T) {
+	is := is.New(t)
+	is.Equal(meander.Cost1, meander.ParseCost("$"))
+	is.Equal(meander.Cost2, meander.ParseCost("$$"))
+	is.Equal(meander.Cost3, meander.ParseCost("$$$"))
+	is.Equal(meander.Cost4, meander.ParseCost("$$$$"))
+	is.Equal(meander.Cost5, meander.ParseCost("$$$$$"))
+}
+
+func TestParseCostRange(t *testing.T) {
+	is := is.New(t)
+	var l meander.CostRange
+	var err error
+	l, err = meander.ParseCostRange("$$...$$$")
+	is.NoErr(err)
+	is.Equal(l.From, meander.Cost2)
+	is.Equal(l.To, meander.Cost3)
+	l, err = meander.ParseCostRange("$...$$$$$")
+	is.NoErr(err)
+	is.Equal(l.From, meander.Cost1)
+	is.Equal(l.To, meander.Cost5)
+}
+func TestCostRangeString(t *testing.T) {
+	is := is.New(t)
+	r := meander.CostRange{
+		From: meander.Cost2,
+		To:   meander.Cost4,
+	}
+	is.Equal("$$...$$$$", r.String())
+}
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+export GOOGLE_CLIENT_SEC="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1"
+cd cmd/meader
+go run main.go
